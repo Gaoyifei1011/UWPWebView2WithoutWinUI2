bf2ae2821e41923d9bc565cda234a67eae7c0d39
高怡飞 <363301617@qq.com>
2024-07-25 10:58:36 +0800

    Initialize UWPWebView2WithoutWinUI2
    Initialize UWPWebView2WithoutWinUI2

---
+++ UWPWebView2WithoutWinUI2/UWPWebView2WithoutWinUI2/Controls/CoreWebView2InitializedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UWPWebView2WithoutWinUI2.Controls
+{
+    /// <summary>
+    /// 为 CoreWebView2Initialized 事件提供数据
+    /// </summary>
+    public class CoreWebView2InitializedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 获取创建 WebView2 时引发的异常。
+        /// </summary>
+        public Exception Exception { get; }
+
+        public CoreWebView2InitializedEventArgs(Exception exception)
+        {
+            Exception = exception;
+        }
+    }
+}
---
+++ UWPWebView2WithoutWinUI2/UWPWebView2WithoutWinUI2/Models/XamlFocusChangeInfoModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Web.WebView2.Core;
+
+namespace UWPWebView2WithoutWinUI2.Models
+{
+    public struct XamlFocusChangeInfoModel
+    {
+        public CoreWebView2MoveFocusReason storedMoveFocusReason;
+
+        public bool isPending;
+    }
+}
